aa378db2da3237b33927da5525e6dc9fd0c2891b
Mac <33965646+SodaDakoda@users.noreply.github.com>
2025-12-02 15:34:14 -0800

    refactored rest URLS to match both fron and back end

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,19 +1,17 @@
 const express = require("express");
 const router = express.Router();
+
 const {
   registerOrganization,
   loginOrganization,
   getMe,
 } = require("../controllers/auth.controller");
+
 const { authRequired } = require("../middleware/auth");
 
-// REGISTER ORG
-router.post("/register-org", registerOrganization);
-
-// LOGIN ORG
-router.post("/login-org", loginOrganization);
-
-// GET CURRENT USER (org admin)
-router.get("/me", authRequired, getMe);
+// ORG AUTH ROUTES
+router.post("/org/register", registerOrganization);
+router.post("/org/login", loginOrganization);
+router.get("/org/me", authRequired, getMe);
 
 module.exports = router;
